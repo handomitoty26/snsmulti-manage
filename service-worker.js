d07615193a6a95dd1f0ef5f0ddd824b5031ab42e
handomitoty26 <cola843@gmail.com>
2026-07-03 22:46:35 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'multi-sns-profit-v8.5.0-dual-ai';
+const CACHE_NAME = 'multi-sns-profit-v8.5.2-auto-latest';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', event => {
